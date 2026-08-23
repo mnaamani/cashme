b09dfbba934eca70e081111b5bb82d82c2f0a439
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2026-08-23 13:14:32 +0400

    move wallet work into lib/ecash.mjs and start adding commands

--- bin.mjs
+++ bin.mjs
@@ -1,4 +1,4 @@
-import { command, flag, summary } from 'paparam'
+import { command, flag, summary, description } from 'paparam'
 import { persistent } from 'bare-storage'
 import process from 'bare-process'
 import os from 'bare-os'
@@ -8,22 +8,20 @@ import FileLog from 'bare-file-logger'
 import Console from 'bare-console'
 import pkg from './package.json'
 import App from './app.js'
-import DHT from 'hyperdht'
-import HttpBridgeClient from '@dhttp/client'
-import 'bare-crypto/global'
-
-// cashu-ts needs global fetch and TextEncoder APIs
-import { TextEncoder } from 'text-encoding'
-globalThis.TextEncoder = TextEncoder
-
-import bareFetch from 'bare-fetch'
-globalThis.fetch = bareFetch
-// import wallet only after setting TextEncoder and fetch on globalThis
-import { Wallet, MintQuoteState } from '@cashu/cashu-ts'
 
 const appName = pkg.productName || pkg.name
 const isDev = path.basename(Bare.argv[0], path.extname(Bare.argv[0])) === 'bare'
 
+const deposit = command(
+  'deposit',
+  description('Deposit ecash into our wallet, by paying a lightning invoice (BOLT11 mint)')
+)
+const give = command('give', description('Send ecash to a neighbour over bluetooth swarm'))
+const get = command('get', description('Receive ecash from a neighbour over bluetooth swarm'))
+const withdraw = command(
+  'withdraw',
+  description('Withdraw ecash from our wallet with lightning invoice payment (BOLT11 melt)')
+)
 const cmd = command(
   appName,
   summary(pkg.description),
@@ -31,7 +29,11 @@ const cmd = command(
   flag('--storage <dir>', 'custom storage directory'),
   flag('--no-updates', 'disable OTA updates for this run'),
   flag('--update-window <ms>', 'updater wait in milliseconds'),
-  flag('--updater', 'run updater daemon').hide()
+  flag('--updater', 'run updater daemon').hide(),
+  deposit,
+  give,
+  get,
+  withdraw
 )
 
 cmd.parse(Bare.argv.slice(isDev ? 2 : 1))
@@ -67,42 +69,6 @@ if (updates !== false) {
     Bare.exit(1)
   }
 }
-
-const testnutBridgeKey = '72c402ee132faddfc8d24141daeed4c91fe5f7ce873d49095f6225330a7b8ba7'
-const dht = new DHT()
-const dhttpclient = new HttpBridgeClient(dht.connect(testnutBridgeKey))
-
-const { upstream } = await dhttpclient.info()
-console.log('Connected to mint bridge:', testnutBridgeKey)
-console.log('Upstream:', upstream)
-
-// now swapout global fetch for the client's fetch, not this means any other part of the app
-// that uses global fetch will be talking to the mint!
-globalThis.fetch = dhttpclient.fetch()
-
-// The real mint url is the upstream the bridge connects to. Passing a different
-// url to the Wallet constructor doesn't change the destination
-// const mintUrl = 'https://testnut.cashu.space'
-const wallet = new Wallet('https://nowhere.invalid', {
-  // These custom transport options don't seem to be working..why?
-  // requestFetch: dhttpclient.fetch(),
-  // customRequest: dhttpclient.fetch(),
-  // OpenId Authentication - needs to reach 'normal' internet, can't use the global fetch
-  // we have overriden to call the mint.
-  oidc: {
-    fetch: bareFetch
-  },
-  unit: 'sat'
-})
-await wallet.loadMint() // wallet is now ready to use
-const mintInfo = wallet.getMintInfo()
-console.log('Wallet Ready.\n')
-console.log(`${mintInfo.name} - ${mintInfo.description}`)
-
-await mintTokens(10)
-
-dhttpclient.destroy()
-await dht.destroy()
 
 async function runUpdater(dir, wait) {
   const app = new App({
@@ -150,22 +116,4 @@ function updateWindow(value) {
     throw new Error('--update-window must be a non-negative integer')
   }
   return wait
-}
-
-async function mintTokens(amount) {
-  console.log(`minting ${amount} tokens`)
-  const mintQuote = await wallet.createMintQuoteBolt11(amount)
-  console.log('invoice', mintQuote)
-  // when using the testnut mint, the bolt11 invoice should be automatically paid on the mint side
-  await sleepSeconds(5)
-  const mintQuoteChecked = await wallet.checkMintQuoteBolt11(mintQuote.quote)
-  console.log(`invoice state: ${mintQuoteChecked.state}`)
-  if (mintQuoteChecked.state === MintQuoteState.PAID) {
-    const proofs = await wallet.mintProofsBolt11(amount, mintQuote.quote)
-    console.log(JSON.stringify(proofs)) // howto safely store the proofs?
-  }
-}
-
-function sleepSeconds(seconds) {
-  return new Promise((resolve) => setTimeout(resolve, seconds * 1000))
 }
